204941fcee23359c9b823303aabd8a76a62192a9
Ernie Casilla <ecasilla@icloud.com>
2014-05-02 16:28:00 -0400

    starting to get parsing working

--- dom.js
+++ dom.js
@@ -15,53 +15,47 @@ readDirFiles.list('Famous.docset/Contents/Resources/Documents',function (err,fil
   }
   //This concat apply merges the two arrays files and filename to one single object key being an index starting at 0 value being the file
     files = files.concat.apply(files,filenames);
-    //loop through all the values in this files object and take their value/filename 
-    for (var i = 0, l = files.length; i < l; i ++) {
-      var filenames = files[i];
-    // remove all css and js files from the filename list
-    if (filenames.substr(-1,1) ==='s' || filenames.substr(-1,1)==='/') {
-        filenames = "" 
-      }
-      console.log(typeof filenames)
-    //   fileReader(filenames);
-      parsedFiles = []
-      parsedFiles = parsedFiles.push(filenames)
-    }
-      console.log(typeof parsedFiles)
+    fileReader(files)
  });
 
 
 function fileReader(file) {
-  console.log(file)
-  if (file === '') {
-    return
-    }
-  fs.readFile(file, 'utf-8',function (err,data) {
-    if (err) {
-      console.log(err)
-    }
-     console.log(data)
-  });
+    console.log('starting to check');
+    var fileNamesArray = (_.values(file))
+    //console.log(fileNamesArray)
+
+    _.each(fileNamesArray,function(index) {
+      console.log(index)
+    if (index === ' ' || index ==='/') {
+
+      }else {
+        fs.readFile(index, 'utf-8',function (err,data) {
+          if (err) {
+            console.log(err)
+          }
+           htmlparse(data)
+        });
+      }
+     });
 }
 
 
-// function htmlparse (file) {
-//  console.log(file)
-//  handler = new htmlparser.DefaultHandler( function (err,dom) {
-//    if (err) {
-//      console.log(err)
-//    }else{
-//    methods = select(dom, 'a.method-tag');
-//    console.log(methods)
-//    methods_.each(function (method) {
-//        sys.puts("Method name is" + " " + method.attribs.name)
-//      });
-//    }
-//  });
-//  
-//parser = new htmlparser.Parser(handler);
-//parser.parseComplete(file);
-// };
+function htmlparse (file) {
+ handler = new htmlparser.DefaultHandler( function (err,dom) {
+   if (err) {
+     console.log(err)
+   }else{
+   var methods = select(dom, 'a.method-tag');
+   console.log(methods)
+   _.each(methods, function (method) {
+       console.log("Method name is" + " " + method.attribs.name)
+     });
+   }
+ });
+
+parser = new htmlparser.Parser(handler);
+parser.parseComplete(file);
+};
 
 
 
